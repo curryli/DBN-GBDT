91bad9f62c449f2f5ec1e0d94749b4eb4d3b178e
curryli <xurui.lee@msn.com>
2016-09-26 19:30:29 +0800

    first commit

---
+++ DBN-GBDT_Done/DBN-GBDT_Done/DBN-GBDT_german/theano-DBN/GBDT.py
@@ -0,0 +1,37 @@
+
+import sys  
+import os  
+import time  
+import numpy as np  
+import cPickle as pickle  
+import numpy as np
+from sklearn import datasets, linear_model, metrics
+import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn.metrics as m 
+  
+
+def classify(X, y):
+    from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor
+    model = GradientBoostingClassifier(n_estimators=200, max_depth=3)
+    model.fit(X, y)
+    return model
+    
+  
+def main():
+    X=[] 
+    y=[]
+
+    X = np.loadtxt("german2.csv",  delimiter = "," , usecols=(range(24)) , dtype=float)
+    
+    y = np.loadtxt("german2.csv",  delimiter = "," , usecols=(24,) , dtype=int)
+ 
+    clf = classify(X, y)
+    print "Done"
+ 
+    print m.classification_report(y, clf.predict(X))
+
+ 
+
+if __name__ == "__main__":
+    main()
